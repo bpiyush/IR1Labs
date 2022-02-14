20f3757730eabf320906a0de4674d53209eba0f6
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2022-02-14 17:38:39 +0100

    Addss empty files for Lab 1

---
+++ lab1/setup.sh
@@ -0,0 +1,12 @@
+# Usage: bash setup.sh
+# Outcome: Creates a conda environment for the lab1 and installs the required packages
+
+echo "::: Creating conda environment for lab1 using conda version $(conda --version)"
+echo "::: This has been tested on Mac OS with I386 architecture."
+
+conda create -y -n ir1-i386
+conda activate ir1-i386
+conda install python=3.6.5 --channel conda-forge
+pip install -r ./requirements.txt
+
+echo "::: Setup complete."
